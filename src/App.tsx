c3f9f2f272b8da692cc68d0e50fc551e23fbe80a
dotegoo <teodordobromir@gmail.com>
2024-01-12 04:59:10 -1200

    'commit'

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+     <header>
+            <div className="title">
+              <h1>Task Manager</h1>
+              <h3>Efficiency Unleashed, Productivity Embraced</h3>
+            </div>
+            <div className="task">
+              <div className="box">
+              <p>Task1</p>
+              <p>Task2</p>
+              <p>Task3</p>
+              </div>
+            </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
