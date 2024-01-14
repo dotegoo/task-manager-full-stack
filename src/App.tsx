fa1675e4fa6f5955d0b9fd7e1d74f309a26f38cc
Teodor <teodordobromir@gmail.com>
2024-01-14 17:44:26 -0500

    14.1.24

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import "./App.css";
 function App() {
-  return (
-    <>
-     <header>
-            <div className="title">
-              <h1>Task Manager</h1>
-              <h3>Efficiency Unleashed, Productivity Embraced</h3>
-            </div>
-            <div className="task">
-              <div className="box">
-              <p>Task1</p>
-              <p>Task2</p>
-              <p>Task3</p>
-              </div>
-            </div>
-      </header>
-    </>
+  return(
+    <header>
+    <div className="title">
+      <h1>Task Manager</h1>
+      <h3>Efficiency Unleashed, Productivity Embraced</h3>
+    </div>
+    <div className="task">
+      <div className="box">
+      <p>Task1</p>
+      <p>Task2</p>
+      <p>Task3</p>
+      </div>
+    </div>
+</header>
   );
 }
 
